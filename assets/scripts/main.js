f1e93ad8fff023d247de96e1e51cca37665dadb3
Hsancore3 <83752900+Hsancore3@users.noreply.github.com>
2025-07-13 13:15:45 +0300

    Update main.js
    - Now on english
    - Download links update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,34 +2,34 @@ const mapsData = [
     {
         id: "ReMountains",
         title: "ReMountains",
-        description: "Переработка карты ReMountains",
+        description: "Reworked Mountains map from S3",
         imageCount: 5,
         preview: "assets/images/ReMountains/image1.png",
-        driveLink: "https://drive.google.com/drive/folders/1abc123mountains"
+        driveLink: "https://drive.google.com/file/d/1QkgrN-Tyz9a3O_do64CfPg2RhDJtXoWK/view?usp=sharing"
     },
     {
         id: "ReMilitaryBase",
         title: "ReMilitaryBase",
-        description: "Переработка Military Base",
+        description: "Reworked Military Base map from S3",
         imageCount: 6,
         preview: "assets/images/ReMilitaryBase/image1.png",
-        driveLink: "https://drive.google.com/drive/folders/2def456military"
+        driveLink: "https://drive.google.com/file/d/1pardfPkhOKIlQscxv40gJRL2rcBmFf8z/view?usp=sharing"
     },
     {
         id: "ReCave",
         title: "ReCave",
-        description: "Переработка карты Cave",
+        description: "Reworked Cave map from S3",
         imageCount: 4,
         preview: "assets/images/ReCave/image1.png",
-        driveLink: "https://drive.google.com/drive/folders/3ghi789cave"
+        driveLink: "https://drive.google.com/file/d/1bILby9ZdawR0xLIhrflSWsHrt9pYzcEP/view?usp=sharing"
     },
     {
         id: "KubikiEx",
         title: "KubikiEx",
-        description: "Кубики",
+        description: "Kubiki. ALMOST everything done using cubes",
         imageCount: 7,
         preview: "assets/images/KubikiEx/image1.png",
-        driveLink: "https://drive.google.com/drive/folders/4jkl012kubiki"
+        driveLink: "https://drive.google.com/file/d/1nrvYY5sOiFCMul1eVrCH2iiFOp9p00Ut/view?usp=sharing"
     }
 ];
 
@@ -53,13 +53,13 @@ function renderMapCards() {
                 <img src="${map.preview}" alt="${map.title} Preview">
                 <div class="overlay">
                     <button class="view-gallery-btn" data-map="${map.id}">
-                        <i class="fas fa-images"></i> Подробнее
+                        <i class="fas fa-images"></i> Gallery
                     </button>
                 </div>
             </div>
             <div class="card-footer">
                 <button class="download-btn" data-link="${map.driveLink}">
-                    <i class="fas fa-download"></i> Скачать карту
+                    <i class="fas fa-download"></i> Download map
                 </button>
             </div>
         `;
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function openGallery(map) {
-    galleryTitle.textContent = `Галерея: ${map.title}`;
+    galleryTitle.textContent = `Gallery: ${map.title}`;
     totalImgs.textContent = map.imageCount;
     updateGalleryImage(map.id, currentIndex);
 
